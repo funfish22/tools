55ad1318ea84b0d7edfbccbd8a1b647e52865dbf
t_aga <aga.lhj@link8tech.tw>
2021-02-02 18:01:59 +0800

    Updates

---
+++ 404.512adf00a6f28ecf74f7.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp_icon=this.webpackJsonpapp_icon||[]).push([[2],{680:function(n,t,e){"use strict";e.r(t);var i=e(52),o=e(10),c=e(53);function r(){var n=Object(i.a)(["\n    position: fixed;\n    background-color: #fff;\n    inset: 0px;\n    color: #38b2a9;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 30px;\n"]);return r=function(){return n},n}t.default=function(){return Object(o.jsx)(a,{children:"Page Not Found"})};var a=c.b.div(r())}}]);
+//# sourceMappingURL=404.512adf00a6f28ecf74f7.js.map
--- 404.d8afbe1117d9f5752973.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp_icon=this.webpackJsonpapp_icon||[]).push([[1],{678:function(n,t,e){"use strict";e.r(t);var i=e(52),o=e(10),c=e(53);function r(){var n=Object(i.a)(["\n    position: fixed;\n    background-color: #fff;\n    inset: 0px;\n    color: #38b2a9;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 30px;\n"]);return r=function(){return n},n}t.default=function(){return Object(o.jsx)(a,{children:"Page Not Found"})};var a=c.b.div(r())}}]);
-//# sourceMappingURL=404.d8afbe1117d9f5752973.js.map
