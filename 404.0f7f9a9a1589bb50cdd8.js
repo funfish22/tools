ee42ab4fffdbd5b487a1ef318f93a24c145c0601
t_aga <aga.lhj@link8tech.tw>
2021-05-05 16:21:58 +0800

    Updates

---
+++ 404.0f7f9a9a1589bb50cdd8.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp_icon=this.webpackJsonpapp_icon||[]).push([[1],{716:function(n,t,e){"use strict";e.r(t);var i=e(58),o=e(9),c=e(60);function r(){var n=Object(i.a)(["\n    position: fixed;\n    background-color: #fff;\n    inset: 0px;\n    color: #38b2a9;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 30px;\n"]);return r=function(){return n},n}t.default=function(){return Object(o.jsx)(a,{children:"Page Not Found"})};var a=c.b.div(r())}}]);
+//# sourceMappingURL=404.0f7f9a9a1589bb50cdd8.js.map
