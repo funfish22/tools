289ed930db7810263537814ed101f7d8182b91dc
funfish22 <j26391442@gmail.com>
2021-05-20 23:24:22 +0800

    Updates

--- 404.4ebe85566d7fcfe0a387.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp_icon=this.webpackJsonpapp_icon||[]).push([[1],{716:function(n,t,e){"use strict";e.r(t);var i=e(58),o=e(9),c=e(60);function r(){var n=Object(i.a)(["\n    position: fixed;\n    background-color: #fff;\n    inset: 0px;\n    color: #38b2a9;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    font-size: 30px;\n"]);return r=function(){return n},n}t.default=function(){return Object(o.jsx)(a,{children:"Page Not Found"})};var a=c.b.div(r())}}]);
-//# sourceMappingURL=404.4ebe85566d7fcfe0a387.js.map
