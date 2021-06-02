aea10623669718a50b0144003d9f44a8951f26ac
t_aga <aga.lhj@link8tech.tw>
2021-06-02 10:09:37 +0800

    優化 loading畫面

--- src/pages/App/Router.js
+++ src/pages/App/Router.js
@@ -1,11 +1,19 @@
 import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import { Spin } from 'antd';
 
 import { routes } from '@route';
 
 function Router() {
     return (
-        <Suspense fallback={<h1>Loading profile...</h1>}>
+        <Suspense
+            fallback={
+                <SpinRoot>
+                    <Spin size="large" />
+                </SpinRoot>
+            }
+        >
             <Switch>
                 {routes.map((row, index) => (
                     <Route key={index} path={row.path} exact={row.exact} component={row.component}></Route>
@@ -16,3 +24,16 @@ function Router() {
 }
 
 export default Router;
+
+const SpinRoot = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 50;
+`;
