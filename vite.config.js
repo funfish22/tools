7c3c4610779518e1fbe479c453cb29db2fcec958
t_aga <aga.lhj@link8tech.tw>
2021-09-02 11:45:38 +0800

    調整 vite部屬到gh-page的跟目錄

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     build: {
         outDir: 'build',
     },
+    base: '/tools/',
     resolve: {
         alias: {
             '@apis': path.resolve(__dirname, './src/services/api'),
