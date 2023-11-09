87dc61a3ceabbc6c63599c9d1f3866058e8046af
t_aga <aga.lhj@link8tech.tw>
2023-11-09 16:46:24 +0800

    新增未來版、修改體育版檔名、全部打包資料夾名稱設定

--- src/pages/default/ResizeIcon/index.jsx
+++ src/pages/default/ResizeIcon/index.jsx
@@ -14,285 +14,302 @@ import Title from '@component/atoms/Title';
 import IconCard from '@component/molecules/IconCard';
 
 const imgSize = [192, 192, 180, 144, 128, 120, 96];
+const imgFolderName = ['標準版', '娛樂版', '體育版', '未來版'];
 
 function ResizeIcon() {
-    const [imageUrl, setImageUrl] = useState('');
-    const [webId, setWebId] = useState('');
-    const [alert, setAlert] = useState(false);
-    const [alertText, setAlertText] = useState('');
-    const [resizeImg, setResizeImg] = useState(false);
-    const [valueVersion, setValueVersion] = useState(1);
-    const [bothVersion, setBothVersion] = useState(false);
+	const [imageUrl, setImageUrl] = useState('');
+	const [webId, setWebId] = useState('');
+	const [alert, setAlert] = useState(false);
+	const [alertText, setAlertText] = useState('');
+	const [resizeImg, setResizeImg] = useState(false);
+	const [valueVersion, setValueVersion] = useState(1);
+	const [bothVersion, setBothVersion] = useState(false);
 
-    const { setH1Title } = useContext(MyContext);
+	const { setH1Title } = useContext(MyContext);
 
-    const uploadButton = (
-        <div>
-            <PlusOutlined />
-            <div style={{ marginTop: 8 }}>上傳圖片</div>
-        </div>
-    );
+	const uploadButton = (
+		<div>
+			<PlusOutlined />
+			<div style={{ marginTop: 8 }}>上傳圖片</div>
+		</div>
+	);
 
-    function getBase64(img, callback) {
-        const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
-        reader.readAsDataURL(img);
+	function getBase64(img, callback) {
+		const reader = new FileReader();
+		reader.addEventListener('load', () => callback(reader.result));
+		reader.readAsDataURL(img);
 
-        reader.onload = function () {
-            const loadImgSize = new Image();
-            loadImgSize.src = reader.result;
-            if (loadImgSize.naturalWidth < imgSize[0] || loadImgSize.naturalHeight < imgSize[0]) {
-                setAlertText(`圖片尺寸錯誤，請上傳${imgSize[0]}x${imgSize[0]}的圖片`);
-                setAlert(true);
-                setImageUrl('');
-            }
-            if (loadImgSize.naturalWidth !== loadImgSize.naturalHeight) {
-                setAlertText(`圖片尺寸錯誤，請上傳${imgSize[0]}x${imgSize[0]}的圖片`);
-                setAlert(true);
-                setImageUrl('');
-            }
-        };
+		reader.onload = function () {
+			const loadImgSize = new Image();
+			loadImgSize.src = reader.result;
+			if (loadImgSize.naturalWidth < imgSize[0] || loadImgSize.naturalHeight < imgSize[0]) {
+				setAlertText(`圖片尺寸錯誤，請上傳${imgSize[0]}x${imgSize[0]}的圖片`);
+				setAlert(true);
+				setImageUrl('');
+			}
+			if (loadImgSize.naturalWidth !== loadImgSize.naturalHeight) {
+				setAlertText(`圖片尺寸錯誤，請上傳${imgSize[0]}x${imgSize[0]}的圖片`);
+				setAlert(true);
+				setImageUrl('');
+			}
+		};
 
-        setTimeout(() => {
-            setAlert(false);
-            setAlertText('');
-        }, 2000);
-    }
+		setTimeout(() => {
+			setAlert(false);
+			setAlertText('');
+		}, 2000);
+	}
 
-    function beforeUpload(file) {
-        const isJpgOrPng = file.type === 'image/png';
-        if (!isJpgOrPng) {
-            message.error('請上傳PNG檔!');
-        }
-        return isJpgOrPng;
-    }
+	function beforeUpload(file) {
+		const isJpgOrPng = file.type === 'image/png';
+		if (!isJpgOrPng) {
+			message.error('請上傳PNG檔!');
+		}
+		return isJpgOrPng;
+	}
 
-    function handleChange(info) {
-        if (info.file.type === 'image/png') {
-            getBase64(info.file.originFileObj, (imageUrl) => {
-                setImageUrl(imageUrl);
-            });
-        }
-    }
+	function handleChange(info) {
+		if (info.file.type === 'image/png') {
+			getBase64(info.file.originFileObj, (imageUrl) => {
+				setImageUrl(imageUrl);
+			});
+		}
+	}
 
-    function dummyRequest(info) {
-        setTimeout(() => {
-            info.onSuccess('ok');
-        }, 0);
-    }
+	function dummyRequest(info) {
+		setTimeout(() => {
+			info.onSuccess('ok');
+		}, 0);
+	}
 
-    function handleChangeID(e) {
-        setWebId(e.target.value);
-    }
+	function handleChangeID(e) {
+		setWebId(e.target.value);
+	}
 
-    function handleRenderImage() {
-        if (!webId) {
-            setAlertText('請輸入站點ID');
-            setAlert(true);
-        } else if (!imageUrl) {
-            setAlertText('請上傳圖片');
-            setAlert(true);
-        } else {
-            setResizeImg(true);
-        }
+	function handleRenderImage() {
+		if (!webId) {
+			setAlertText('請輸入站點ID');
+			setAlert(true);
+		} else if (!imageUrl) {
+			setAlertText('請上傳圖片');
+			setAlert(true);
+		} else {
+			setResizeImg(true);
+		}
 
-        setTimeout(() => {
-            setAlert(false);
-            setAlertText('');
-        }, 2000);
-    }
+		setTimeout(() => {
+			setAlert(false);
+			setAlertText('');
+		}, 2000);
+	}
 
-    function handleAlertClose() {
-        setAlert(false);
-    }
+	function handleAlertClose() {
+		setAlert(false);
+	}
 
-    function handleReset() {
-        setImageUrl('');
-        setWebId('');
-        setResizeImg(false);
-    }
+	function handleReset() {
+		setImageUrl('');
+		setWebId('');
+		setResizeImg(false);
+	}
 
-    function handleChangeVersion(e) {
-        setValueVersion(e.target.value);
-    }
+	function handleChangeVersion(e) {
+		setValueVersion(e.target.value);
+	}
 
-    function handleBothVersion() {
-        setValueVersion(1);
-        setBothVersion(!bothVersion);
-    }
+	function handleBothVersion() {
+		setValueVersion(1);
+		setBothVersion(!bothVersion);
+	}
 
-    function handleSave() {
-        const zip = new JSZip();
-        let src = '';
-        let name = '';
-        for (let i = 0; i < imgSize.length; i++) {
-            src = document
-                .getElementById(`resizeImg${i + 1}`)
-                .getAttribute('src')
-                .replace('data:image/png;base64,', '');
-            if (bothVersion) {
-                for (let j = 0; j < 2; j++) {
-                    if (j === 0) {
-                        name = document
-                            .getElementById(`iconName${i + 1}`)
-                            .textContent.replace('app_icon_', 'app_icon_effects_');
-                    } else {
-                        name = document.getElementById(`iconName${i + 1}`).textContent;
-                    }
-                    zip.file(name, src, { base64: true });
-                    if (j === 0) {
-                        name = document
-                            .getElementById(`iconName${i + 1}`)
-                            .textContent.replace('app_icon_', 'app_icon_sport_');
-                    } else {
-                        name = document.getElementById(`iconName${i + 1}`).textContent;
-                    }
-                    zip.file(name, src, { base64: true });
-                }
-            } else {
-                name = document.getElementById(`iconName${i + 1}`).textContent;
-                zip.file(name, src, { base64: true });
-            }
-        }
-        zip.generateAsync({ type: 'blob' }).then(function (content) {
-            if (bothVersion) {
-                saveAs(content, `${webId}站點 標準版、娛樂版、體育版APP ICON`);
-                return;
-            }
-            if (valueVersion === 1) {
-                saveAs(content, `${webId}站點 標準版APP ICON`);
-            }
-            if (valueVersion === 2) {
-                saveAs(content, `${webId}站點 娛樂版APP ICON`);
-            }
-            if (valueVersion === 3) {
-                saveAs(content, `${webId}站點 體育版APP ICON`);
-            }
-        });
-    }
+	function handleSave() {
+		const zip = new JSZip();
+		let src = '';
+		let name = '';
+		for (let i = 0; i < imgSize.length; i++) {
+			src = document
+				.getElementById(`resizeImg${i + 1}`)
+				.getAttribute('src')
+				.replace('data:image/png;base64,', '');
+			if (bothVersion) {
+				for (let j = 0; j < 4; j++) {
+					let imgFolder = zip.folder(imgFolderName[j]);
+					if (j === 0) {
+						name = document.getElementById(`iconName${i + 1}`).textContent;
+					}
+					if (j === 1) {
+						// imgFolder ;
+						name = document
+							.getElementById(`iconName${i + 1}`)
+							.textContent.replace('app_icon_', 'app_icon_effects_');
+					}
+					if (j === 2) {
+						name = document.getElementById(`iconName${i + 1}`).textContent;
+						if (name.includes(webId)) {
+							name = document
+								.getElementById(`iconName${i + 1}`)
+								.textContent.replace('app_icon_', 'app_icon_sport_');
+						}
+					}
+					if (j === 3) {
+						name = document.getElementById(`iconName${i + 1}`).textContent;
+						if (name.includes(webId)) {
+							name = document
+								.getElementById(`iconName${i + 1}`)
+								.textContent.replace('app_icon_', 'app_icon_future_');
+						}
+					}
+					imgFolder.file(name, src, { base64: true });
+					// zip.file(name, src, { base64: true });
+				}
+			} else {
+				name = document.getElementById(`iconName${i + 1}`).textContent;
+				zip.file(name, src, { base64: true });
+			}
+		}
+		zip.generateAsync({ type: 'blob' }).then(function (content) {
+			if (bothVersion) {
+				saveAs(content, `${webId}站點 標準版、娛樂版、體育版、未來版APP ICON`);
+				return;
+			}
+			if (valueVersion === 1) {
+				saveAs(content, `${webId}站點 標準版APP ICON`);
+			}
+			if (valueVersion === 2) {
+				saveAs(content, `${webId}站點 娛樂版APP ICON`);
+			}
+			if (valueVersion === 3) {
+				saveAs(content, `${webId}站點 體育版APP ICON`);
+			}
+			if (valueVersion === 4) {
+				saveAs(content, `${webId}站點 未來版APP ICON`);
+			}
+		});
+	}
 
-    function resizeImageUrl(size) {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        const resizeImg = new Image();
+	function resizeImageUrl(size) {
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+		const resizeImg = new Image();
 
-        resizeImg.src = imageUrl;
-        canvas.width = size;
-        canvas.height = size;
+		resizeImg.src = imageUrl;
+		canvas.width = size;
+		canvas.height = size;
 
-        ctx.drawImage(resizeImg, 0, 0, size, size);
-        const dataURL = canvas.toDataURL('image/png');
+		ctx.drawImage(resizeImg, 0, 0, size, size);
+		const dataURL = canvas.toDataURL('image/png');
 
-        return dataURL;
-    }
+		return dataURL;
+	}
 
-    useEffect(() => {
-        setH1Title('APP ICON 圖片尺寸調整工具');
-    });
+	useEffect(() => {
+		setH1Title('APP ICON 圖片尺寸調整工具');
+	});
 
-    return (
-        <ResizeIconRoot>
-            {alert && <AlertRoot type="error" message={alertText} banner closable onClose={handleAlertClose} />}
-            <ResizeIconHeader>
-                <Form>
-                    <Row gutter={32}>
-                        <Col span={10}>
-                            <TitleRoot size={20} borderBottom>
-                                上傳圖片
-                            </TitleRoot>
-                            <UploadRoot
-                                listType="picture-card"
-                                showUploadList={false}
-                                beforeUpload={beforeUpload}
-                                onChange={handleChange}
-                                customRequest={dummyRequest}
-                            >
-                                {imageUrl ? (
-                                    <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
-                                ) : (
-                                    uploadButton
-                                )}
-                            </UploadRoot>
-                        </Col>
-                        <Col span={10}>
-                            <TitleRoot size={20} borderBottom>
-                                圖片資訊
-                            </TitleRoot>
-                            <InputRoot>
-                                <p>站點ID</p>
-                                <Input size="small" type="number" value={webId} onChange={handleChangeID} />
-                            </InputRoot>
-                            <InputRoot>
-                                <p>版本</p>
-                                {!bothVersion && (
-                                    <Radio.Group value={valueVersion} onChange={handleChangeVersion}>
-                                        <Radio value={1}>標準版</Radio>
-                                        <Radio value={2}>娛樂版</Radio>
-                                        <Radio value={3}>體育版</Radio>
-                                    </Radio.Group>
-                                )}
-                                <Checkbox checked={bothVersion} onChange={handleBothVersion}>
-                                    全部
-                                </Checkbox>
-                            </InputRoot>
-                        </Col>
-                        <Col span={4} style={{ display: 'flex', flexDirection: 'column' }}>
-                            <TitleRoot size={20} borderBottom>
-                                功能
-                            </TitleRoot>
-                            <Button type="primary" size="large" block onClick={handleRenderImage}>
-                                執行圖片縮放
-                            </Button>
-                            <Button
-                                type="danger"
-                                size="large"
-                                block
-                                style={{ marginTop: '10px' }}
-                                onClick={handleReset}
-                            >
-                                重置
-                            </Button>
-                            <Button
-                                type="primary"
-                                size="large"
-                                block
-                                style={{ marginTop: 'auto' }}
-                                href="https://tinypng.com/"
-                                target="_blank"
-                            >
-                                TinyPng網站
-                            </Button>
-                            <Button
-                                type="primary"
-                                size="large"
-                                block
-                                style={{ marginTop: '10px' }}
-                                onClick={handleSave}
-                            >
-                                打包下載
-                            </Button>
-                        </Col>
-                    </Row>
-                </Form>
-            </ResizeIconHeader>
-            <ResizeIconBody>
-                <Row gutter={[16, 16]}>
-                    {resizeImg &&
-                        imgSize.map((row, index) => (
-                            <Col span={6} key={row}>
-                                <IconCard
-                                    id={index}
-                                    size={[row, row]}
-                                    src={resizeImageUrl(row)}
-                                    version={valueVersion}
-                                    webId={webId}
-                                />
-                            </Col>
-                        ))}
-                </Row>
-            </ResizeIconBody>
-        </ResizeIconRoot>
-    );
+	return (
+		<ResizeIconRoot>
+			{alert && <AlertRoot type="error" message={alertText} banner closable onClose={handleAlertClose} />}
+			<ResizeIconHeader>
+				<Form>
+					<Row gutter={32}>
+						<Col span={10}>
+							<TitleRoot size={20} borderBottom>
+								上傳圖片
+							</TitleRoot>
+							<UploadRoot
+								listType="picture-card"
+								showUploadList={false}
+								beforeUpload={beforeUpload}
+								onChange={handleChange}
+								customRequest={dummyRequest}
+							>
+								{imageUrl ? (
+									<img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
+								) : (
+									uploadButton
+								)}
+							</UploadRoot>
+						</Col>
+						<Col span={10}>
+							<TitleRoot size={20} borderBottom>
+								圖片資訊
+							</TitleRoot>
+							<InputRoot>
+								<p>站點ID</p>
+								<Input size="small" type="number" value={webId} onChange={handleChangeID} />
+							</InputRoot>
+							<InputRoot>
+								<p>版本</p>
+								{!bothVersion && (
+									<Radio.Group value={valueVersion} onChange={handleChangeVersion}>
+										<Radio value={1}>標準版</Radio>
+										<Radio value={2}>娛樂版</Radio>
+										<Radio value={3}>體育版</Radio>
+										<Radio value={4}>未來版</Radio>
+									</Radio.Group>
+								)}
+								<Checkbox checked={bothVersion} onChange={handleBothVersion}>
+									全部
+								</Checkbox>
+							</InputRoot>
+						</Col>
+						<Col span={4} style={{ display: 'flex', flexDirection: 'column' }}>
+							<TitleRoot size={20} borderBottom>
+								功能
+							</TitleRoot>
+							<Button type="primary" size="large" block onClick={handleRenderImage}>
+								執行圖片縮放
+							</Button>
+							<Button
+								type="danger"
+								size="large"
+								block
+								style={{ marginTop: '10px' }}
+								onClick={handleReset}
+							>
+								重置
+							</Button>
+							<Button
+								type="primary"
+								size="large"
+								block
+								style={{ marginTop: 'auto' }}
+								href="https://tinypng.com/"
+								target="_blank"
+							>
+								TinyPng網站
+							</Button>
+							<Button
+								type="primary"
+								size="large"
+								block
+								style={{ marginTop: '10px' }}
+								onClick={handleSave}
+							>
+								打包下載
+							</Button>
+						</Col>
+					</Row>
+				</Form>
+			</ResizeIconHeader>
+			<ResizeIconBody>
+				<Row gutter={[16, 16]}>
+					{resizeImg &&
+						imgSize.map((row, index) => (
+							<Col span={6} key={row}>
+								<IconCard
+									id={index}
+									size={[row, row]}
+									src={resizeImageUrl(row)}
+									version={valueVersion}
+									webId={webId}
+								/>
+							</Col>
+						))}
+				</Row>
+			</ResizeIconBody>
+		</ResizeIconRoot>
+	);
 }
 
 export default memo(ResizeIcon);
@@ -300,50 +317,50 @@ export default memo(ResizeIcon);
 const ResizeIconRoot = styled.main``;
 
 const ResizeIconHeader = styled.section`
-    padding: 30px 0;
-    background-color: #ccc;
+	padding: 30px 0;
+	background-color: #ccc;
 
-    form {
-        max-width: 1000px;
-        margin: 0 auto;
-    }
+	form {
+		max-width: 1000px;
+		margin: 0 auto;
+	}
 `;
 
 const UploadRoot = styled(Upload)`
-    .ant-upload-select-picture-card {
-        width: 192px;
-        height: 192px;
-        margin-bottom: 40px;
-    }
+	.ant-upload-select-picture-card {
+		width: 192px;
+		height: 192px;
+		margin-bottom: 40px;
+	}
 `;
 
 const TitleRoot = styled(Title)`
-    margin-bottom: 10px;
+	margin-bottom: 10px;
 `;
 
 const InputRoot = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    p {
-        min-width: 80px;
-        margin-right: 10px;
-        margin-bottom: 0;
-    }
-    input {
-        width: 170px;
-    }
+	display: flex;
+	align-items: center;
+	margin-bottom: 10px;
+	p {
+		min-width: 80px;
+		margin-right: 10px;
+		margin-bottom: 0;
+	}
+	input {
+		width: 170px;
+	}
 `;
 
 const AlertRoot = styled(Alert)`
-    position: fixed;
-    top: 20px;
-    left: 50%;
-    transform: translateX(-50%);
+	position: fixed;
+	top: 20px;
+	left: 50%;
+	transform: translateX(-50%);
 `;
 
 const ResizeIconBody = styled.section`
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 30px 0;
+	max-width: 1000px;
+	margin: 0 auto;
+	padding: 30px 0;
 `;
--- src/resources/component/molecules/IconCard/index.jsx
+++ src/resources/component/molecules/IconCard/index.jsx
@@ -4,124 +4,134 @@ import styled from 'styled-components';
 import { Input } from 'antd';
 
 function IconCard(props) {
-    const { size, src, id, webId, version, BatchImg, name, multiple, changeName, onChangeName } = props;
-    function reName() {
-        if (size[0] !== 128) {
-            if (version === 1) {
-                if (id !== 1) {
-                    return `app_icon_${size[0]}x${size[1]}.png`;
-                } else {
-                    return `app_icon_${webId}_c.png`;
-                }
-            }
-            if (version === 2) {
-                if (id !== 1) {
-                    return `app_icon_effects_${size[0]}x${size[0]}.png`;
-                } else {
-                    return `app_icon_effects_${webId}_c.png`;
-                }
-            }
-            if (version === 3) {
-                if (id !== 1) {
-                    return `app_icon_sport_${size[0]}x${size[0]}.png`;
-                } else {
-                    return `app_icon_sport_${webId}_c.png`;
-                }
-            }
-        } else {
-            if (version === 1) {
-                return `app_icon_${webId}.png`;
-            }
-            if (version === 2) {
-                return `app_icon_effects_${webId}.png`;
-            }
-            if (version === 3) {
-                return `app_icon_sport_${webId}.png`;
-            }
-        }
-        if (size === 150) {
-            return `${webId}_qrcode.png`;
-        }
-        if (BatchImg) {
-            return `${name} ${multiple}`;
-        }
-    }
-    return (
-        <IconCardRoot>
-            <h3>
-                {size[0]} x {size[1]}
-            </h3>
-            <div className="img_img">
-                <img src={src} id={'resizeImg' + (id + 1)} alt={name} />
-            </div>
-            {changeName ? (
-                <>
-                    <p
-                        className={`${!BatchImg ? 'card_name' : 'card_name card_name--batch'}`}
-                        id={'iconName' + (id + 1)}
-                    >
-                        {reName()}
-                    </p>{' '}
-                    <Input size="small" style={{ marginTop: '11.2px' }} onChange={onChangeName} />
-                </>
-            ) : (
-                <p className={`${!BatchImg ? 'card_name' : 'card_name card_name--batch'}`} id={'iconName' + (id + 1)}>
-                    {reName()}
-                </p>
-            )}
-        </IconCardRoot>
-    );
+	const { size, src, id, webId, version, BatchImg, name, multiple, changeName, onChangeName } = props;
+	function reName() {
+		if (size[0] !== 128) {
+			if (version === 1) {
+				if (id !== 1) {
+					return `app_icon_${size[0]}x${size[1]}.png`;
+				} else {
+					return `app_icon_${webId}_c.png`;
+				}
+			}
+			if (version === 2) {
+				if (id !== 1) {
+					return `app_icon_effects_${size[0]}x${size[0]}.png`;
+				} else {
+					return `app_icon_effects_${webId}_c.png`;
+				}
+			}
+			if (version === 3) {
+				if (id !== 1) {
+					return `app_icon_${size[0]}x${size[0]}.png`;
+				} else {
+					return `app_icon_sport_${webId}_c.png`;
+				}
+			}
+			if (version === 4) {
+				if (id !== 1) {
+					return `app_icon_${size[0]}x${size[0]}.png`;
+				} else {
+					return `app_icon_future_${webId}_c.png`;
+				}
+			}
+		} else {
+			if (version === 1) {
+				return `app_icon_${webId}.png`;
+			}
+			if (version === 2) {
+				return `app_icon_effects_${webId}.png`;
+			}
+			if (version === 3) {
+				return `app_icon_sport_${webId}.png`;
+			}
+			if (version === 4) {
+				return `app_icon_future_${webId}.png`;
+			}
+		}
+		if (size === 150) {
+			return `${webId}_qrcode.png`;
+		}
+		if (BatchImg) {
+			return `${name} ${multiple}`;
+		}
+	}
+	return (
+		<IconCardRoot>
+			<h3>
+				{size[0]} x {size[1]}
+			</h3>
+			<div className="img_img">
+				<img src={src} id={'resizeImg' + (id + 1)} alt={name} />
+			</div>
+			{changeName ? (
+				<>
+					<p
+						className={`${!BatchImg ? 'card_name' : 'card_name card_name--batch'}`}
+						id={'iconName' + (id + 1)}
+					>
+						{reName()}
+					</p>{' '}
+					<Input size="small" style={{ marginTop: '11.2px' }} onChange={onChangeName} />
+				</>
+			) : (
+				<p className={`${!BatchImg ? 'card_name' : 'card_name card_name--batch'}`} id={'iconName' + (id + 1)}>
+					{reName()}
+				</p>
+			)}
+		</IconCardRoot>
+	);
 }
 
 export default memo(IconCard);
 
 const IconCardRoot = styled.div`
-    text-align: center;
-    background-color: #fff;
-    border-radius: 4px;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+	text-align: center;
+	background-color: #fff;
+	border-radius: 4px;
+	padding: 10px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
 
-    .img_img {
-        width: 192px;
-        height: 192px;
-        position: relative;
-    }
+	.img_img {
+		width: 192px;
+		height: 192px;
+		position: relative;
+	}
 
-    h3 {
-        font-size: 20px;
-    }
+	h3 {
+		font-size: 20px;
+	}
 
-    img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        max-width: 100%;
-        max-height: 100%;
-    }
+	img {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		max-width: 100%;
+		max-height: 100%;
+	}
 
-    p {
-        margin-top: 10px;
-        margin-bottom: 0;
-        font-size: 16px;
-    }
+	p {
+		margin-top: 10px;
+		margin-bottom: 0;
+		font-size: 16px;
+	}
 
-    .card_name {
-        max-width: 218px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        position: relative;
-        &--batch {
-            padding-right: 20px;
-            &:after {
-                position: absolute;
-                content: '倍';
-                right: 0;
-            }
-        }
-    }
+	.card_name {
+		max-width: 218px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		position: relative;
+		&--batch {
+			padding-right: 20px;
+			&:after {
+				position: absolute;
+				content: '倍';
+				right: 0;
+			}
+		}
+	}
 `;
